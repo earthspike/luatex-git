b327bca3d12469956594922af64ec74f10a6789d
Earthspike <github@earthspike.net>
2018-02-19 23:27:02 +0000

    Refactor the macro setting element to avoid TeX encoding dangers
    The previous version was ugly and dangerous. By collecting each git log
    parameter in its own popen, and using tex.?print(-2, _) mode, we can avoid
    the danger of non-catcode 10 and 12 characters. This has only removed the
    danger, not the ugly. @FIXME: map ascii characters to the correct TeX
    encoding, eg \ needs to map to \ not `.

--- luagitsty.lua
+++ luagitsty.lua
@@ -4,13 +4,6 @@
 --
 --[[
  Sets the following TeX variables:
-  \ifingit = true if in a git repository
-  \gitbranch = name of (local) git branch
-  \lastgitcommitref = git commit reference
-  \lastgitcommitdate = git commit date
-  \lastgitcommitname = Author name
-  \lastgitcommitemail = Author email
-  \ifchangedsincegitcommit = true if changed files in working area since commit
 
 Example git commands
 -------------------
@@ -63,6 +56,28 @@ On branch master
 nothing to commit, working directory clean
 --]]
 
+function print_git_commit_parameter_def(param, tex_macro)
+   -- Calls git log for an individual parameter <param> and sets the <tex_macro> to the output
+   local handle = io.popen("git log -1 --pretty=format:\"" .. param .. "\"")
+   local result = handle:read("*a")
+   local success, err = handle:close()
+
+   if success then
+      lines = {}
+      result:gsub("[^\10]+", function(l)
+		     lines[#lines + 1] = l
+			     end)
+      tex.sprint("\\def\\" .. tex_macro .. "{")
+      tex.print(-2,lines)
+      tex.print("}")
+   else
+      tex.sprint("\\def\\" .. tex_macro .. "{")
+      tex.sprint(-2, err) -- Render err as catcode 12 except for spaces (catcode 10)
+      tex.print("}")
+   end
+end
+      
+
 local handle = io.popen("git status")
 local result = handle:read("*a")
 local success, err = handle:close()
@@ -95,6 +110,43 @@ if success then
       tex.print("\\changedsincegitcommitfalse")
    end
 
+   print_git_commit_parameter_def("%H","gitcommithash")
+   print_git_commit_parameter_def("%h","gitcommithashshort")
+   print_git_commit_parameter_def("%T","gitcommittreehash")
+   print_git_commit_parameter_def("%t","gitcommittreehashshort")
+   print_git_commit_parameter_def("%P","gitcommitparenthashes")
+   print_git_commit_parameter_def("%p","gitcommitparenthashesshort")
+   print_git_commit_parameter_def("%an","gitcommitauthorname")
+   print_git_commit_parameter_def("%aN","gitcommitauthornamemapped")
+   print_git_commit_parameter_def("%ae","gitcommitauthoremail")
+   print_git_commit_parameter_def("%aE","gitcommitauthoremailmapped")
+   print_git_commit_parameter_def("%ai","gitcommitauthordate")
+   print_git_commit_parameter_def("%cn","gitcommitcommittername")
+   print_git_commit_parameter_def("%cN","gitcommitcomitternamemapped")
+   print_git_commit_parameter_def("%ce","gitcommitcommitteremail")
+   print_git_commit_parameter_def("%cE","gitcommitcommitteremailmapped")
+   print_git_commit_parameter_def("%ci","gitcommitcommitterdate")
+   print_git_commit_parameter_def("%d","gitcommitrefnames")
+   print_git_commit_parameter_def("%e","gitcommitencoding")
+   print_git_commit_parameter_def("%s","gitcommitsubject")
+   print_git_commit_parameter_def("%f","gitcommitsubjectsanitised")
+   print_git_commit_parameter_def("%b","gitcommitbody")
+   print_git_commit_parameter_def("%B","gitcommitbodyraw")
+   print_git_commit_parameter_def("%N","gitcommitnotes")
+   print_git_commit_parameter_def("%G?","gitcommitsignaturestatus")
+   print_git_commit_parameter_def("%GS","gitcommitsigner")
+   print_git_commit_parameter_def("%GK","gitcommitkey")
+   print_git_commit_parameter_def("%gD","gitcommitreflogselector")
+   print_git_commit_parameter_def("%gd","gitcommitreflogselectorshort")
+   print_git_commit_parameter_def("%gn","gitcommitreflogidentityname")
+   print_git_commit_parameter_def("%gN","gitcommitreflogidentitynamemapped")
+   print_git_commit_parameter_def("%ge","gitcommitreflogidentityemail")
+   print_git_commit_parameter_def("%gE","gitcommitreflogidentityemailmapped")
+   print_git_commit_parameter_def("%gs","gitcommitreflogsubject")
+
+   
+   
+--[=[
    -- Now get details of the last commit from `git log --pretty:format` using this format:
    local gitlogformat = "\\def\\gitcommithash{%H}\\def\\gitcommithashshort{%h}%n" ..
    	 "\\def\\gitcommittreehash{%T}\\def\\gitcommittreehashshort{%t}%n" ..
@@ -122,7 +174,7 @@ if success then
       result:gsub("[^\10]+", function(l)
 		     lines[#lines + 1] = l
 			     end)
-      for i=1,#lines do tex.print(lines[i]) end
+      tex.print(lines)
 --[[
       -- Get the commit reference 
       gitcommit = lines[1]:match("commit (%x+)")
@@ -138,6 +190,7 @@ if success then
       -- No git log (empty repo)
       tex.print("\\def\\lastgitcommithash{(Empty repository)}")
    end
+--]=]
 end
 
 --[==[  From git log --help:
